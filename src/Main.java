7d252cb5f1e08492ae3fd0f4d0e57c89998874c4
Piotr <P.Larkowski90@gmail.com>
2024-03-18 13:12:14 +0100

    Add change from kebab-case to camel-case

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println(SqueakyClean.clean("Ala ma kota"));
+        System.out.println(SqueakyClean.clean("Ala-ma-kota"));
     }
 }
--- src/SqueakyClean.java
+++ src/SqueakyClean.java
@@ -2,10 +2,13 @@ public class SqueakyClean {
     static String clean(String identifier) {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append(identifier);
-
-        for (int i = 0; i < identifier.length(); i++) {
+        for (int i = 0; i < stringBuilder.length(); i++) {
             if (stringBuilder.charAt(i) == ' ') {
                 stringBuilder.replace(i,i+1,"_");
+            }
+            if (stringBuilder.charAt(i) == '-') {
+                char letter = stringBuilder.charAt(i+1);
+                stringBuilder.replace(i,i+2,""+Character.toUpperCase(letter));
             }
         }
         return stringBuilder.toString();
