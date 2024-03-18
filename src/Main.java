e994ef13b45349bdbc33c5f0e9deaa81bf769db6
Piotr <P.Larkowski90@gmail.com>
2024-03-18 14:21:27 +0100

    Add checking does input String have other character than letter if that so application delete it

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println(SqueakyClean.clean("a$#.b"));
         System.out.println(SqueakyClean.clean("Ala ma kota"));
         System.out.println(SqueakyClean.clean("Ala-ma-kota"));
         System.out.println(SqueakyClean.clean("¡1337sp34k is fun!"));
--- src/SqueakyClean.java
+++ src/SqueakyClean.java
@@ -1,56 +1,73 @@
 public class SqueakyClean {
     static String clean(String identifier) {
-            StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.append(identifier);
-
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(identifier);
+        boolean isInArray = true;
+        char[] allowedLetterArray = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','r','s','t','u'
+                ,'w','z','x','y','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','R','S','T','U','W',
+                'Z','X','Y','-','0','1','3','4','7','!','¡',' '};
         for (int i = 0; i < stringBuilder.length(); i++) {
-                if (stringBuilder.charAt(i) == ' ') {
-                    stringBuilder.replace(i,i+1,"_");
-                    i--;
-                    continue;
-                }
-                if (stringBuilder.charAt(i) == '-') {
-                    char letter = stringBuilder.charAt(i+1);
-                    stringBuilder.replace(i,i+2,""+Character.toUpperCase(letter));
-                    i--;
-                    continue;
-                }
-                if (stringBuilder.charAt(i) == '0') {
-                    stringBuilder.replace(i,i+1,"o");
-                    i--;
-                    continue;
-                }
-                if (stringBuilder.charAt(i) == '1') {
-                    stringBuilder.replace(i,i+1,"l");
-                    i--;
-                    continue;
-                }
-                if (stringBuilder.charAt(i) == '3') {
-                    stringBuilder.replace(i,i+1,"e");
-                    i--;
-                    continue;
-                }
-                if (stringBuilder.charAt(i) == '4') {
-                    stringBuilder.replace(i,i+1,"a");
-                    i--;
-                    continue;
-                }
-                if (stringBuilder.charAt(i) == '7') {
-                    stringBuilder.replace(i,i+1,"t");
-                    i--;
-                    continue;
-                }
-                if (stringBuilder.charAt(i) == '!') {
-                    stringBuilder.replace(i,i+1,"");
-                    i--;
-                    continue;
-                }
-                if (stringBuilder.charAt(i) == '¡') {
-                    stringBuilder.replace(i,i+1,"");
-                    i--;
-                    continue;
+            for (int j = 0; j < allowedLetterArray.length; j++) {
+                if(stringBuilder.charAt(i)!=allowedLetterArray[j]){
+                    isInArray = false;
+                }else{
+                    isInArray = true;
+                    break;
                 }
             }
-            return stringBuilder.toString();
+            if(!isInArray){
+                stringBuilder.replace(i, i + 1, "");
+                i--;
+            }
+            isInArray = true;
+        }
+        for (int i = 0; i < stringBuilder.length(); i++) {
+            if (stringBuilder.charAt(i) == ' ') {
+                stringBuilder.replace(i, i + 1, "_");
+                i--;
+                continue;
+            }
+            if (stringBuilder.charAt(i) == '-') {
+                char letter = stringBuilder.charAt(i + 1);
+                stringBuilder.replace(i, i + 2, "" + Character.toUpperCase(letter));
+                i--;
+                continue;
+            }
+            if (stringBuilder.charAt(i) == '0') {
+                stringBuilder.replace(i, i + 1, "o");
+                i--;
+                continue;
+            }
+            if (stringBuilder.charAt(i) == '1') {
+                stringBuilder.replace(i, i + 1, "l");
+                i--;
+                continue;
+            }
+            if (stringBuilder.charAt(i) == '3') {
+                stringBuilder.replace(i, i + 1, "e");
+                i--;
+                continue;
+            }
+            if (stringBuilder.charAt(i) == '4') {
+                stringBuilder.replace(i, i + 1, "a");
+                i--;
+                continue;
+            }
+            if (stringBuilder.charAt(i) == '7') {
+                stringBuilder.replace(i, i + 1, "t");
+                i--;
+                continue;
+            }
+            if (stringBuilder.charAt(i) == '!') {
+                stringBuilder.replace(i, i + 1, "");
+                i--;
+                continue;
+            }
+            if (stringBuilder.charAt(i) == '¡') {
+                stringBuilder.replace(i, i + 1, "");
+                i--;
+            }
+        }
+        return stringBuilder.toString();
     }
 }
