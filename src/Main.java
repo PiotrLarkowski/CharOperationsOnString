03f74d77d79cbec9d36c104b3ebb6cb0ec1f41c1
Piotr <P.Larkowski90@gmail.com>
2024-03-18 13:35:39 +0100

    replace leetspeak to normal letters

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(SqueakyClean.clean("Ala ma kota"));
         System.out.println(SqueakyClean.clean("Ala-ma-kota"));
+        System.out.println(SqueakyClean.clean("¡1337sp34k is fun!"));
     }
 }
--- src/SqueakyClean.java
+++ src/SqueakyClean.java
@@ -1,16 +1,56 @@
 public class SqueakyClean {
     static String clean(String identifier) {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(identifier);
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append(identifier);
+
         for (int i = 0; i < stringBuilder.length(); i++) {
-            if (stringBuilder.charAt(i) == ' ') {
-                stringBuilder.replace(i,i+1,"_");
+                if (stringBuilder.charAt(i) == ' ') {
+                    stringBuilder.replace(i,i+1,"_");
+                    i--;
+                    continue;
+                }
+                if (stringBuilder.charAt(i) == '-') {
+                    char letter = stringBuilder.charAt(i+1);
+                    stringBuilder.replace(i,i+2,""+Character.toUpperCase(letter));
+                    i--;
+                    continue;
+                }
+                if (stringBuilder.charAt(i) == '0') {
+                    stringBuilder.replace(i,i+1,"o");
+                    i--;
+                    continue;
+                }
+                if (stringBuilder.charAt(i) == '1') {
+                    stringBuilder.replace(i,i+1,"l");
+                    i--;
+                    continue;
+                }
+                if (stringBuilder.charAt(i) == '3') {
+                    stringBuilder.replace(i,i+1,"e");
+                    i--;
+                    continue;
+                }
+                if (stringBuilder.charAt(i) == '4') {
+                    stringBuilder.replace(i,i+1,"a");
+                    i--;
+                    continue;
+                }
+                if (stringBuilder.charAt(i) == '7') {
+                    stringBuilder.replace(i,i+1,"t");
+                    i--;
+                    continue;
+                }
+                if (stringBuilder.charAt(i) == '!') {
+                    stringBuilder.replace(i,i+1,"");
+                    i--;
+                    continue;
+                }
+                if (stringBuilder.charAt(i) == '¡') {
+                    stringBuilder.replace(i,i+1,"");
+                    i--;
+                    continue;
+                }
             }
-            if (stringBuilder.charAt(i) == '-') {
-                char letter = stringBuilder.charAt(i+1);
-                stringBuilder.replace(i,i+2,""+Character.toUpperCase(letter));
-            }
-        }
-        return stringBuilder.toString();
+            return stringBuilder.toString();
     }
 }
